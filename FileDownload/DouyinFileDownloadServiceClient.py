89f35900e3bda243448170b7632851e5a1677eb9
Zeng, Boling <zengboling@gmail.com>
2024-08-05 13:52:09 +0800

    video filename

--- FileDownload/DouyinFileDownloadServiceClient.py
+++ FileDownload/DouyinFileDownloadServiceClient.py
@@ -1,6 +1,7 @@
 import asyncio
 import re
 from datetime import datetime
+from unittest import IsolatedAsyncioTestCase
 
 import aiohttp
 
@@ -23,8 +24,8 @@ class DouyinFileDownloadServiceClient(object):
             return False
 
     async def download_file(self, video_url, nickname):
-        server_url = "http://h8.9zma.com:8099/"
-        time_str = datetime.now().strftime("%H-%M-%S")
+        server_url = "http://ha8d.9zma.com:8099/"
+        time_str = datetime.now().strftime("%d %H_%M_%S")
         filename = f'{nickname[:40]}-{time_str}.mp4'
         filename = re.sub(r'[\\/*?:"<>|#\n\r]', '', filename)
         folder = "C:\\Users\\Admin\\Nox_share\\ImageShare\\"
@@ -39,3 +40,14 @@ class DouyinFileDownloadServiceClient(object):
 
     def start_download_file(self, video_url, nickname):
         asyncio.create_task(self.download_file(video_url, nickname))
+
+class TestDouyinFileDownloadServiceClient(IsolatedAsyncioTestCase):
+    async def test(self):
+        server_url='http://ha8d.9zma.com:8099/'
+        localDir = 'C:\\Users\\Admin\\Nox_share\\ImageShare\\1.mp4'
+        video_url='https://api-hl.amemv.com/aweme/v1/play/?video_id=v0d00fg10000cqkv6jvog65s0qrcb0m0&line=1&file_id=d24baa0efc194c65b7733eea4cbcf24e&sign=cd38c5d23949913249314650070c2de3&is_play_url=1&source=PackSourceEnum_PUBLISH'
+        await DouyinFileDownloadServiceClient().download_file_remotely(server_url, video_url, localDir)
+
+    def test_time(self):
+        time_str = datetime.now().strftime("%d %H_%M_%S")
+        print(time_str)
